9ad1649017b0cee95e9df4f77a8ffbd260158445
AnhQuoc <phamanhquoc2014@gmail.com>
2018-11-15 03:02:55 +0700

    Thêm tương tác với mấy cái nút

--- Project_DoHoa2D/Project_DoHoa2D/Form1.cs
+++ Project_DoHoa2D/Project_DoHoa2D/Form1.cs
@@ -14,12 +14,17 @@ namespace Project_DoHoa2D
 {
     public partial class Save : Form
     {
+        List<Button> shapes = new List<Button>();
+        List<Button> tools = new List<Button>();
+        List<Button> colors = new List<Button>();
+        String[] colorList = { "Black", "Silver","Gray", "White", "Maroon","Red","Purple","Fuchsia",
+            "Green","Lime","Olive","Yellow","Navy","Blue","Teal","Aqua"};
+        int currentShape, currentTool, currentColor;
         public Save()
         {
             InitializeComponent();
-
-            List<Button> shapes = new List<Button>();
-            #region Add Button Shape
+     
+            #region Add Shape Buttons
             shapes.Add(btnLine);
             shapes.Add(btnRectangle);
             shapes.Add(btnCircle);
@@ -29,30 +34,88 @@ namespace Project_DoHoa2D
             shapes.Add(btnZigzag);
             shapes.Add(btnArc);
             #endregion
-            
-            List<Button> tools = new List<Button>();
-            #region Add Button Tool
+                 
+            #region Add Tool Buttons
             tools.Add(btnFill);
             tools.Add(btnMove);
             tools.Add(btnRotate);
             tools.Add(btnScale);
             #endregion
-            
-            String[] colors = { "Black", "Silver","Gray", "White", "Maroon","Red","Purple","Fuchsia",
-            "Green","Lime","Olive","Yellow","Navy","Blue","Teal","Aqua"};
+
+            #region Add Color Buttons
+            for (int i = 0; i < colorList.Length; i++)
+                colors.Add(
+                    (Button)this.Controls.Find("btnColor" + colorList[i], true)[0]
+                    );
+            #endregion
 
             #region Set Default Value
             cboDashstyle.SelectedIndex = 0;
             nudWitdh.Value = 1;
-            int currentShape = 0; //Line
-            shapes[currentShape].BackColor = Color.AliceBlue;
-            int currentTool = -1; //Nothing
-            int currentColor = 0;
-            btnCurrentColor.BackColor = Color.FromName(colors[currentColor]);
+            currentShape = 0; //Line
+            shapes[currentShape].BackColor = CONST.COLOR_CURRENT_SHAPE;
+            currentTool = -1; //Nothing
+            currentColor = 0;
+            btnCurrentColor.BackColor = Color.FromName(colorList[currentColor]);
             #endregion
 
             pnlRotateAngel.Visible = false;
             pnlAdjustScale.Visible = false;
+        }
+
+        private void updatePanelToolChildren(int currentTool)
+        {
+
+        }
+
+        private void button_Shape_Click(object sender, EventArgs e)
+        {
+            if (currentTool != -1)
+            {
+                tools[currentTool].BackColor = Color.White;
+                currentTool = -1;
+                pnlAdjustScale.Visible = false;
+                pnlRotateAngel.Visible = false;
+            }
+           
+            Button clikedShapeButton = sender as Button;
+            shapes[currentShape].BackColor = Color.Transparent;
+            for (int i = 0; i < shapes.Count; i++)
+            if (clikedShapeButton == shapes[i])
+            {
+                currentShape = i;
+                shapes[currentShape].BackColor = CONST.COLOR_CURRENT_SHAPE;
+                break;
+            }
+        }
+
+        private void button_Tool_Click(object sender, EventArgs e)
+        {
+            Button clikedButton = sender as Button;
+            if (currentTool != -1)
+                tools[currentTool].BackColor = Color.White;
+            for(int i = 0; i < tools.Count; i++)
+                if(clikedButton == tools[i])
+                {
+                    currentTool = i;
+                    tools[currentTool].BackColor = CONST.COLOR_CURRENT_TOOL;
+                    break;
+                }
+
+            pnlRotateAngel.Visible = (clikedButton == btnRotate);
+            pnlAdjustScale.Visible = (clikedButton == btnScale);
+        }
+
+        private void button_Color_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            for (int i = 0; i < colors.Count; i++)
+                if (clickedButton == colors[i])
+                {
+                    currentColor = i;
+                    btnCurrentColor.BackColor = Color.FromName(colorList[i]);
+                    break;
+                }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
